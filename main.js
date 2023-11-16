3b80cf80fb3e46d7bb84c4795f1b011e580b77d3
mathiashc99 <mathiasHC@live.dk>
2023-11-16 13:20:05 +0100

    added search bar

--- main.js
+++ main.js
@@ -1,46 +1,61 @@
-const ctx = document.querySelector("#bar-test")
-const yLabels = data.map((row)=>row.Airport)
-const bgColorArray = []
+async function getData() {
+    try {
+        const response = await fetch('data.json');
+        const textData = await response.text();
+        return JSON.parse(textData);
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        return [];
+    }
+}
 
-data.forEach((obj,index)=> {
-    obj.Airport == "UNKNOWN" ? bgColorArray[index]="grey" : bgColorArray[index]="#8C0383"
-})
+function displayResults(matchingAirports) {
+    const resultsContainer = document.getElementById('results');
+    resultsContainer.innerHTML = '';
+
+    if (matchingAirports.length === 0) {
+        resultsContainer.innerHTML = 'No results found. Sorry!';
+    } else {
+        matchingAirports.forEach(airport => {
+            const airportElement = document.createElement('div');
+            airportElement.textContent = airport.Airport;
+            resultsContainer.appendChild(airportElement);
+        });
+    }
+}
+
+function search() {
+    const searchTerm = document.getElementById('searchInput').value.trim().toLowerCase();
+    let matchingAirports;
+
+    if (searchTerm.length === 1) {
+        matchingAirports = airports.filter(airport =>
+            airport.Airport.toLowerCase().startsWith(searchTerm)
+        );
+    } else {
+        matchingAirports = airports.filter(airport =>
+            airport.Airport.toLowerCase().includes(searchTerm)
+        );
+    }
+
+    displayResults(matchingAirports);
+}
 
 
+function clearSearch() {
+    document.getElementById('searchInput').value = '';
+    document.getElementById('results').innerHTML = '';
+}
 
-const myChart = new Chart(ctx,
-    {
-        type:'bar',
-        data:{
-            labels:yLabels,
-            datasets:[
-                {
-                    label:'Total cost per Airport',
-                    data: data.map(row=>row.total),
-                    backgroundColor: bgColorArray,
-                }
-            ]
-        },
-        options:{
-            maintainAspectRatio: false,
-            indexAxis:"y",
-            plugins:{
-                title:{
-                    display:true,
-                    text:"The Cost",
-                    font:{
-                        size:22,
-                        weight: "bold",
-                    }
-                }
-            },
-            scales:{
-                y:{
+let airports = []; // Define the variable for airports
 
-                },
-                x:{
-
-                },
-            },
-        }
+getData().then(data => {
+    airports = data;
+}).catch(error => {
+    console.error('Error fetching data:', error);
 });
+
+// Attach event listeners
+document.getElementById('searchInput').addEventListener('input', search);
+document.getElementById('searchButton').addEventListener('click', search);
+document.getElementById('clearButton').addEventListener('click', clearSearch);
