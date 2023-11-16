1861e76715063abf90739295571de562beee9ea7
SushiGirrl <lykkeflor@gmail.com>
2023-11-16 12:33:23 +0100

    added eventlisteners added launch button drawScene function dot functions

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const canvas = document.querySelector("canvas");
 const ctx= canvas.getContext("2d");
+const launch = document.querySelector(".missiles");
 
 //Lykke
 // Defining airplane shape
@@ -74,4 +75,168 @@ function drawAirplane() {
     ctx.fillStyle = "#7C9498FF"; // Green color
     ctx.fill();
 }
-drawAirplane();
+
+function drawDot(x, y, dotSize, dotColor) {
+    ctx.fillStyle = dotColor;
+    ctx.beginPath();
+    ctx.arc(x, y, dotSize, 0, Math.PI * 2);
+    ctx.fill();
+}
+
+// Function to draw dots within the circular shape of the cockpit with a delay
+function drawDotsInCockpitWithDelay() {
+    const cockpitCenterX = airplane.x + 100;
+    const cockpitCenterY = airplane.y + 15;
+    const cockpitRadius = 24;
+
+    let count = 0;
+
+    function drawDotInCockpitWithDelay() {
+        if (count < 50) {
+            const angle = Math.random() * Math.PI * 2;
+            const distance = Math.random() * cockpitRadius;
+            const dotX = cockpitCenterX + distance * Math.cos(angle);
+            const dotY = cockpitCenterY + distance * Math.sin(angle);
+
+            drawDot(dotX, dotY, 2, 'red');
+            count++;
+
+            // Adjust the delay time (in milliseconds) based on your preference
+            setTimeout(drawDotInCockpitWithDelay, 50);
+        }
+    }
+
+    drawDotInCockpitWithDelay();
+}
+
+// Function to draw dots within the circular shape of the motor with a delay
+function drawDotsInMotorWithDelay(centerX, centerY) {
+    const motorRadius = 10;
+    let count = 0;
+
+    function drawDotInMotorWithDelay() {
+        if (count < 20) {
+            const angle = Math.random() * Math.PI * 2;
+            const distance = Math.random() * motorRadius;
+            const dotX = centerX + distance * Math.cos(angle);
+            const dotY = centerY + distance * Math.sin(angle);
+
+            drawDot(dotX, dotY, 2, 'red');
+            count++;
+
+            // Adjust the delay time (in milliseconds) based on your preference
+            setTimeout(drawDotInMotorWithDelay, 50);
+        }
+    }
+
+    drawDotInMotorWithDelay();
+}
+
+// Function to draw dots within the rectangular shape of the wings with a delay
+function drawDotsInWingsWithDelay() {
+    const wingWidth = 200;
+    const wingHeight = 7;
+    let count = 0;
+
+    function drawDotInWingsWithDelay() {
+        if (count < 100) {
+            const dotX = Math.random() * wingWidth + airplane.x;
+            const dotY = Math.random() * wingHeight + airplane.y + 20;
+
+            drawDot(dotX, dotY, 2, 'red');
+            count++;
+
+            // Adjust the delay time (in milliseconds) based on your preference
+            setTimeout(drawDotInWingsWithDelay, 50);
+        }
+    }
+
+    drawDotInWingsWithDelay();
+}
+
+// Function to draw dots within the triangular shape with a delay
+function drawDotsInTriangleWithDelay(x1, y1, x2, y2, x3, y3) {
+    let count = 0;
+
+    function drawDotWithDelay() {
+        if (count < 20) {
+            const u = Math.random();
+            const v = Math.random();
+
+            const w1 = 1 - Math.sqrt(u);
+            const w2 = (1 - v) * Math.sqrt(u);
+            const w3 = v * Math.sqrt(u);
+
+            const dotX = w1 * x1 + w2 * x2 + w3 * x3;
+            const dotY = w1 * y1 + w2 * y2 + w3 * y3;
+
+            drawDot(dotX, dotY, 2, 'red');
+            count++;
+
+            // Adjust the delay time (in milliseconds) based on your preference
+            setTimeout(drawDotWithDelay, 50);
+        }
+    }
+
+    drawDotWithDelay();
+}
+
+function birdAttack() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawAirplane();
+
+    // Draw dots within the circular shape of the cockpit with a delay
+    drawDotsInCockpitWithDelay();
+
+    // Draw dots within the circular shape of the left motor with a delay
+    drawDotsInMotorWithDelay(airplane.x + 55, airplane.y + 35);
+
+    // Draw dots within the circular shape of the right motor with a delay
+    drawDotsInMotorWithDelay(airplane.x + 145, airplane.y + 35);
+
+    // Draw dots within the rectangular shape of the wings with a delay
+    drawDotsInWingsWithDelay();
+
+    // Draw dots within the triangular shape of the right wing-tip with a delay
+    drawDotsInTriangleWithDelay(
+        airplane.x + 200, airplane.y + 20,
+        airplane.x + 250, airplane.y + 20,
+        airplane.x + 200, airplane.y + 27
+    );
+
+    // Draw dots within the triangular shape of the left wing-tip with a delay
+    drawDotsInTriangleWithDelay(
+        airplane.x - 50, airplane.y + 20,
+        airplane.x, airplane.y + 20,
+        airplane.x, airplane.y + 27
+    );
+
+    // Draw dots within the triangular shape of the tail middle with a delay
+    drawDotsInTriangleWithDelay(
+        airplane.x + 100, airplane.y - 40,
+        airplane.x + 103, airplane.y - 9,
+        airplane.x + 97, airplane.y - 9
+    );
+
+    // Draw dots within the triangular shape of the tail right with a delay
+    drawDotsInTriangleWithDelay(
+        airplane.x + 150, airplane.y + 3,
+        airplane.x + 100, airplane.y + 8,
+        airplane.x + 100, airplane.y + 15
+    );
+
+    // Draw dots within the triangular shape of the tail left with a delay
+    drawDotsInTriangleWithDelay(
+        airplane.x + 50, airplane.y + 3,
+        airplane.x + 100, airplane.y + 8,
+        airplane.x + 100, airplane.y + 15
+    );
+}
+
+window.addEventListener("load", ()=>{
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawAirplane();
+})
+launch.addEventListener("click", () =>{
+    birdAttack();
+})
